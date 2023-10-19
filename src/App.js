e9a123e1bd58f9c5f179610d9397cc7ad0ca808c
LANCE FRANCISCO <lancecff26@gmail.com>
2023-10-19 21:04:26 +0800

    Initial code(Routing)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/LoginPage';
+import Dashboard from './components/DashBoard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+            <Route index path='/' element={<Login></Login>}></Route>
+            <Route path='dashboard' element={<Dashboard></Dashboard>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
