2626cb586a285a567cf0f7bfe1f0722f45323340
jtidio <145768126+jtidio@users.noreply.github.com>
2023-10-26 20:19:27 +0800

    testing adding channels

---
+++ src/Services/ChannelService.jsx
@@ -0,0 +1,29 @@
+import { API_URL } from '../constants/Constants';
+import axios from 'axios';
+
+const ChannelService = {
+    // Object method for getting users
+    getChannels: async function(user) {
+        try{
+            const response = await axios.get(`${API_URL}/channels`,{
+
+                headers:{
+                    "access-token": user.accessToken,
+                    expiry: user.expiry,
+                    client: user.client,
+                    uid: user.uid
+                }
+            })
+            
+            const channels = response.data.data;
+            return channels.filter((channel) => channel.id >= 5000);
+        } catch(error){
+            if(error.response.data.errors){
+                return alert("Cannot get channels");
+            }
+        }
+    }
+
+}
+
+export default ChannelService;
